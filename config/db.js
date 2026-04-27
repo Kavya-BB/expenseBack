cb42741552aae69584fc515c737e912ae818d5f3
kavya <kavyabanni29@gmail.com>
2026-04-28 01:22:29 +0530

    update file

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 async function configureDB() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/pgfinder-april2025');
+        // await mongoose.connect('mongodb://127.0.0.1:27017/pgfinder-april2025');
+        await mongoose.connect(process.env.DB_URL);
         console.log('connected to db');
     } catch(err) {
         console.log('error connecting to db', err.message);
