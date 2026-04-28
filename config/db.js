d60bc59d004636191c1c806652e4c16e0684e90e
kavya <kavyabanni29@gmail.com>
2026-04-28 12:44:39 +0530

    updated files

--- config/db.js
+++ config/db.js
@@ -4,6 +4,11 @@ async function configureDB() {
     try {
         // await mongoose.connect('mongodb://127.0.0.1:27017/pgfinder-april2025');
         await mongoose.connect(process.env.DB_URL);
+        // await mongoose.connect(process.env.DB_URL, {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true
+        // });
+        // console.log(process.env.DB_URL);
         console.log('connected to db');
     } catch(err) {
         console.log('error connecting to db', err.message);
